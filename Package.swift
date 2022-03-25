601a77b722fb46216d44b846ab741b584429eef7
Nelly <nelly@gokatch.com>
2022-03-25 16:53:33 -0400

    use the tag again

--- Package.swift
+++ Package.swift
@@ -11,12 +11,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AmazonChimeSDK",
-            url: "https://github.com/gokatch/amazon-chime-spm/raw/main/AmazonChimeSDK.xcframework.zip",
+            url: "https://github.com/gokatch/amazon-chime-spm/releases/download/v0.19.2/AmazonChimeSDK.xcframework.zip",
             checksum: "cc7cd2f951c17c43a497d2f703db7daa3ccd7195e40fc83fd3e4687882b7da46"
         ),
         .binaryTarget(
             name: "AmazonChimeSDKMedia",
-            url: "https://github.com/gokatch/amazon-chime-spm/raw/main/AmazonChimeSDKMedia.xcframework.zip",
+            url: "https://github.com/gokatch/amazon-chime-spm/releases/download/v0.19.2/AmazonChimeSDKMedia.xcframework.zip",
             checksum: "66eb7af5ffdeabfbcc9b42a7e8e34348607c2cb166a39449fa6dabf18ec0c77f"
         )
     ]
